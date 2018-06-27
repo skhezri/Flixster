c03a5c9dee4477af412d96e1692177817f79a504
Sophia Khezri <skhezri@fb.com>
2018-06-27 11:09:03 -0700

    Initial Commit

---
+++ Flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixster
+//
+//  Created by Sophia Khezri on 6/27/18.
+//  Copyright © 2018 Sophia Khezri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
